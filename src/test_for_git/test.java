f8743805cd93ff17ed7a2e3482ce1531264e9aee
smallbailangui <lin31415@qq.com>
2025-06-16 23:27:36 +0800

    测试多个分支的git操作

---
+++ src/test_for_git/test.java
@@ -0,0 +1,10 @@
+package test_for_git;
+
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is a test from git branch");
+    }
+}
