03bd427d2c0b3875b426a838c43614fac2a96959
smallbailangui <lin31415@qq.com>
2025-06-16 13:59:21 +0800

    注释修改

--- src/main/java/runApplication.java
+++ src/main/java/runApplication.java
@@ -3,9 +3,9 @@ import main.java.GUI.*;
 
 public class runApplication {
     public static void main(String[] args) throws Exception {
-        //下面是改用单例模式设计的用户界面
+//        下面是改用单例模式设计的用户界面
 //        CinemaController.getInstance().start();
-        //new CinemaController().start();
+//        new CinemaController().start();
         Main.getM().main();
     }
 }
